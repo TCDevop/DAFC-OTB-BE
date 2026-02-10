d43ef70690b733620c7faab03bee267553d0ca74
Phuoc Van <phuoc.van@tcdata.vn>
2026-02-10 10:53:04 +0700

    chore: ignore dist folder

--- dist/src/common/guards/permissions.guard.d.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
-export declare const PERMISSIONS_KEY = "permissions";
-export declare const RequirePermissions: (...permissions: string[]) => import("@nestjs/common").CustomDecorator<string>;
-export declare class PermissionsGuard implements CanActivate {
-    private reflector;
-    constructor(reflector: Reflector);
-    canActivate(context: ExecutionContext): boolean;
-}
